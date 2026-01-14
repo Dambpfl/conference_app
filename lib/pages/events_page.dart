24a2e0127a3f818c875fb4bf47a384df06f9d463
Damien <boenapfel.damien@yahoo.fr>
2026-01-14 17:43:04 +0100

    Add : ListView (planning)

--- lib/pages/events_page.dart
+++ lib/pages/events_page.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
 
-class EventPage extends StatelessWidget {
+class EventPage extends StatefulWidget {
   const EventPage({super.key});
 
   @override
+  State<EventPage> createState() => _EventPageState();
+}
+
+class _EventPageState extends State<EventPage> {
+
+  final events = [
+    {
+      "speaker": "Github",
+      "date": "13h à 13h30",
+      "subject": "Qu'est-ce que c'est ?",
+      "avatar": "github"
+    },
+    {
+      "speaker": "Symfony",
+      "date": "17h à 18h",
+      "subject": "Le framework PHP",
+      "avatar": "symfony"
+    },
+    {
+      "speaker": "Tailwind",
+      "date": "18h à 18h30",
+      "subject": "La bibliothèque CSS",
+      "avatar": "tailwind"
+    }
+  ];
+
+    @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Planning du salon"),
       ),
       body: Center(
-        child: Text("Prochainement disponible"),
+        child: ListView.builder(
+          itemCount: events.length,
+          itemBuilder: (context, index) {
+            final event = events[index];
+            final avatar = event["avatar"];
+            final speaker = event["speaker"];
+            final date = event["date"];
+            final subject = event["subject"];
+
+            return Card(
+            child: ListTile(
+              leading: Image.asset("assets/images/$avatar.png", width: 50, height: 50),
+              title: Text('$speaker ($date)'),
+              subtitle: Text('$subject'),
+              trailing: Icon(Icons.info),
+            ),
+          );
+          },
+        ),
       ),
     );
   }
