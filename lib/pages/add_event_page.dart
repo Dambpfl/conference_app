11f7abb74fde9e8a1de6f6b634554c2912f8df82
Damien <boenapfel.damien@yahoo.fr>
2026-01-15 18:14:46 +0100

    Add : Form + validator

--- lib/pages/add_event_page.dart
+++ lib/pages/add_event_page.dart
@@ -1,12 +1,137 @@
+import 'package:date_field/date_field.dart';
 import 'package:flutter/material.dart';
 
-class AddEventPage extends StatelessWidget {
+class AddEventPage extends StatefulWidget {
   const AddEventPage({super.key});
 
   @override
+  State<AddEventPage> createState() => _AddEventPageState();
+}
+
+class _AddEventPageState extends State<AddEventPage> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  final confNameController = TextEditingController();
+  final speakerNameController = TextEditingController();
+  String selectedConfType = 'talk';
+  DateTime? selectedConfDate;
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    confNameController.dispose();
+    speakerNameController.dispose();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Formulaire d'ajout d'event"),
-      );
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Nom Conference',
+                  hintText: 'Entre le nom de la conférence',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if(value == null || value.isEmpty) {
+                    return "Tu dois completer ce texte";
+                  }
+                  return null;
+                },
+                controller: confNameController,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Nom du speaker',
+                  hintText: 'Entre le nom du speaker',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if(value == null || value.isEmpty) {
+                    return "Tu dois completer ce texte";
+                  }
+                  return null;
+                },
+                controller: speakerNameController,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              child: DropdownButtonFormField(
+                items: [
+                  DropdownMenuItem(value: 'talk', child: Text("Talk show"),),
+                  DropdownMenuItem(value: 'demo', child: Text("Demo code"),),
+                  DropdownMenuItem(value: 'partner', child: Text("Partner"),)
+                ],
+                decoration: InputDecoration(
+                  border: OutlineInputBorder()
+                ),
+                value: selectedConfType,
+                onChanged: (value) {
+                  setState(() {
+                    selectedConfType = value!;
+                  });
+                }
+                ),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              child: DateTimeFormField(
+                decoration: const InputDecoration(
+                  hintStyle: TextStyle(color: Colors.black45),
+                  errorStyle: TextStyle(color: Colors.redAccent),
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(Icons.event_note),
+                  labelText: 'Choisir une date',
+                ),
+                mode: DateTimeFieldPickerMode.dateAndTime,
+                autovalidateMode: AutovalidateMode.always,
+                validator: (e) => (e?.day ?? 0) == 1 ? 'Please not the first day' : null,
+                onChanged: (DateTime? value) {
+                  setState(() {
+                    selectedConfDate = value;
+                  });
+                },
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: (){
+                  if(_formKey.currentState!.validate()) {
+                    final confName = confNameController.text;
+                    final speakerName = speakerNameController.text;
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Envoi en cours.."))
+                    );
+                    FocusScope.of(context).requestFocus(FocusNode());
+
+                    print("Ajout de la conf $confName par le speaker $speakerName");
+                    print("Type de conference $selectedConfType");
+                    print("Date de la conf $selectedConfDate");
+                  }
+                }, 
+                child: Text("Envoyer")
+                ),
+            )
+          ],
+        )
+      ),
+    );
   }
 }
